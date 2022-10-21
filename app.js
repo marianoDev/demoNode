a878b5c9dec9a3ea4a8b89240eeb0d7916f2ee65
Juan <juan.alesandri.acc@gmail.com>
2022-10-21 13:47:38 -0300

    Web server para proyecto de recetas

--- app.js
+++ app.js
@@ -8,18 +8,12 @@ port = process.env.PORT;
 app.use(express.static('public'));
 
 //Rutas
-app.get('/generic', (req, res) => {
-    res.sendFile(`${__dirname}/public/generic.html`);
-})
-
-app.get('/elements', (req, res) => {
-    res.sendFile(`${__dirname}/public/elements.html`);
-})
-
-app.get('*', (req, res) => {
-    res.sendFile(`${__dirname}/public/404.html`);
-})
+// app.get('*', (req, res) => {
+//     res.sendFile(`${__dirname}/public/index.html`);
+// })
 
 app.listen(port, () => {
     console.log('Server on port', port);
 });
+
+//https://webserver-nodejpa.herokuapp.com
