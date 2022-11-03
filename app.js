9d8436a3e27facf05e6bc8b29c8e159bee2d0ee4
Juan <juan.alesandri.acc@gmail.com>
2022-11-03 17:27:44 -0300

    Creacion ruta signup

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
-const express = require('express');
 require('dotenv').config();
-const app = express();
+const Server = require('./models/server');
 
-port = process.env.PORT;
+const server = new Server();
 
-//Middleware para servir contenido estatico
-app.use(express.static('public'));
-
-//Rutas
-// app.get('*', (req, res) => {
-//     res.sendFile(`${__dirname}/public/index.html`);
-// })
-
-app.listen(port, () => {
-    console.log('Server on port', port);
-});
+server.listen();
